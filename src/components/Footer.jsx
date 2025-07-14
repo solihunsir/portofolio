4a163faa26a10be6f8047a3f40b994ad93f48fec
Muhammad Sholihun <solihun.bks2019@gmail.com>
2025-07-14 21:20:45 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+const Footer = () => {
+  return (
+    <div className="mt-32 py-4 flex md:flex-row flex-col gap-6 md:gap-0 justify-between items-center">
+      <h1 className="text-2xl font-bold">Portfolio</h1>
+      <div className="flex gap-7">
+        <a href="#beranda">Beranda</a>
+        <a href="#tentang">Tentang</a>
+        <a href="#proyek">Proyek</a>
+      </div>
+      <div className="flex items-center gap-3">
+        <a href="https://github.com/solihunsir">
+          <i className="ri-github-fill ri-2x"></i>
+        </a>
+        <a href="https://www.instagram.com/solihunsir">
+          <i className="ri-instagram-fill ri-2x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/m-sholihun">
+          <i className="ri-linkedin-fill ri-2x"></i>
+        </a>
+        <a href="https://www.youtube.com/@solihunsir">
+          <i className="ri-youtube-fill ri-2x"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,147 @@
+import HeroImage from "/assets/conixx.jpg";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools3 from "/assets/tools/flutter.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools9 from "/assets/tools/android.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/figma.png";
+import Tools12 from "/assets/tools/mysql.png";
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
+    nama: "Flutter",
+    ket: "Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 9,
+    gambar: Tools9,
+    nama: "Android Studio",
+    ket: "Emulator",
+    dad: "900",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Figma",
+    ket: "Design App",
+    dad: "1100",
+  },
+  {
+    id: 12,
+    gambar: Tools12,
+    nama: "MySQL",
+    ket: "Database",
+    dad: "1100",
+  },
+];
+
+import Proyek1 from "/assets/proyek/proyek1.png";
+import Proyek2 from "/assets/proyek/proyek2.png";
+import Proyek3 from "/assets/proyek/proyek3.png";
+import Proyek4 from "/assets/proyek/proyek4.png";
+
+export const listProyek = [
+  {
+    id: 1,
+    gambar: Proyek1,
+    nama: "Aplikasi Hotel Bengkalis",
+    desk: "Aplikasi yang berfungsi mencari Hotel dan Wisma terdekat dari posisi pengguna",
+    tools: ["Dart", "Google Maps", "Google Street View"],
+    dad: "200",
+  },
+  {
+    id: 2,
+    gambar: Proyek2,
+    nama: "Judi Guard",
+    desk: "Website untuk mencegah dan memberantas Komentar Judi Online di Platform Youtube ",
+    tools: ["ReactJS", "Express"],
+    dad: "300",
+  },
+  {
+    id: 3,
+    gambar: Proyek3,
+    nama: "Portal Berita",
+    desk: "Website Portal Berita yang memberikan berita yang terbaru dan relevan ",
+    tools: ["Laravel", "TailwindCSS"],
+    dad: "400",
+  },
+  {
+    id: 4,
+    gambar: Proyek4,
+    nama: "Agenda Kelas",
+    desk: "Website berfungsi untuk memberikan Informasi kegiatan dan agenda selama perkuliahan",
+    tools: ["Html", "CSS", "Bootstrap"],
+    dad: "500",
+  },
+];
